b52e9036277221dc4d09580bc815b2bfd8338950
junpeidayo <ht23a047@oecu.jp>
2025-05-14 19:32:09 +0900

    TypeScript

---
+++ 04-typescript/03-function-class/type-params.ts
@@ -0,0 +1,8 @@
+interface CustomError<E = Error> {
+        type: 'syntax' | 'logic' | 'runtime';
+        error :E;
+ }
+    const tokenError: CustomError = {
+        type: 'syntax',
+        error: new Error('Unexpected Token'),
+ };
---
+++ 04-typescript/05-advanced/pick-omit.ts
@@ -0,0 +1,7 @@
+interface Todo {
+    title: string;
+    description: string;
+    isDone: boolean;
+}
+type PickedTodo = Pick<Todo, 'title' | 'isDone'>;
+type OmittedTodo = Omit<Todo, 'description'>;
